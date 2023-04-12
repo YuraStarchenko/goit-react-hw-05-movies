5fc79f5588bed589af90f9de471eddc83aea05f7
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-12 15:17:19 +0300

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
+
 const Home = () => {
-	
+  useEffect(() => {
+    // https://api.themoviedb.org/3/movie/550?api_key=fcfa30881c3e984af96d841d25a29426
+  });
   return <div>Trending today 🤷‍♂️</div>;
 };
 
