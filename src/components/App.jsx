2ee3148700281ea5e63db642d1bad1062d46940b
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-13 08:30:02 +0300

    🔞

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import { GlobalStyle } from 'GlobalStyle';
 import { Container } from './Container.styled.js';
 import Home from '../pages/Home/Home.jsx';
 import Movies from '../pages/Movies/Movies.jsx';
+import MovieDetails from '../pages/Movies/MovieDetails/MovieDetails';
+import Cast from 'pages/Movies/MovieDetails/Cast/Cast.jsx';
+import Reviews from 'pages/Movies/MovieDetails/Reviews/Reviews.jsx';
 
 export const App = () => {
   return (
@@ -13,7 +16,14 @@ export const App = () => {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/movies" element={<Movies />} />
+
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+				</Route>
+				
       </Routes>
       <GlobalStyle />
     </Container>
