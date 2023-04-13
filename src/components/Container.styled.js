06d7d066a3f79db492c218c5c1a3b667f95d13a9
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-13 12:11:05 +0300

    Update Container.styled.js

--- src/components/Container.styled.js
+++ src/components/Container.styled.js
@@ -1,9 +1,3 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  ${'' /* display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 40px; */}
-`;
+export const Container = styled.div``;
