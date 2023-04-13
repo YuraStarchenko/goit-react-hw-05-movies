0bfea8f000ea0a24c812fcdde0aa11696888d169
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-13 14:40:33 +0300

    Update movieApi.jsx

--- src/service/movieApi.jsx
+++ src/service/movieApi.jsx
@@ -8,26 +8,26 @@ export const getTrendingMuvies = async () => {
   return data;
 };
 
-// export const getTrendingMuvies = async () => {
-//   const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
-//   const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
-//   return data;
-// };
+export const getSearchMovies = async () => {
+  const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+  const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
+  return data;
+};
 
-// export const getTrendingMuvies = async () => {
-//   const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
-//   const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
-//   return data;
-// };
+export const getDetailsMovie = async () => {
+  const BASE_URL = 'https://api.themoviedb.org/3/movie';
+  const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
+  return data;
+};
 
-// export const getTrendingMuvies = async () => {
-//   const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
-//   const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
-//   return data;
-// };
+export const getCreditsMovie = async () => {
+  const BASE_URL = 'https://api.themoviedb.org/3/movie';
+  const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
+  return data;
+};
 
-// export const getTrendingMuvies = async () => {
-//   const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
-//   const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
-//   return data;
-// };
+export const getReviewsMuvies = async () => {
+  const BASE_URL = 'https://api.themoviedb.org/3/movie';
+  const { data } = await axios.get(`${BASE_URL}/?${KEY}`);
+  return data;
+};
