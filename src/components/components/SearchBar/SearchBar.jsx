9fe3691bcc05da59bf4583329d9915f3d32488c3
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-13 07:51:07 +0300

    +

---
+++ src/components/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Header, Form, Button, Input } from './SearchBar.styled.js';
+import { BsSearch } from 'react-icons/bs';
+
+export const SearchBar = ({ onSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const inputHandleChange = e => {
+    setInputValue(e.currentTarget.value.toLowerCase());
+  };
+
+  // Не дает засобмитить пустой инпут и пробелы
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (inputValue.trim() === '') {
+      Notify.info('Enter the name of the picture 🌅');
+      return;
+    }
+    onSubmit(inputValue);
+    setInputValue('');
+  };
+
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">
+          <BsSearch />
+        </Button>
+
+        <Input
+          type="text"
+          autocomplete="off"
+          autoFocus
+          value={inputValue}
+          placeholder="Search images and photos"
+          onChange={inputHandleChange}
+        />
+      </Form>
+    </Header>
+  );
+};
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -1,52 +1,10 @@
-import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Header, Form, Button, Input } from './Movies.styled.js';
-import { BsSearch } from 'react-icons/bs';
+import { useEffect } from 'react';
 
-const Movies = ({ onSubmit }) => {
-  const [inputValue, setInputValue] = useState('');
-
+const Movies = () => {
   useEffect(() => {
     // https://api.themoviedb.org/3/movie/550?api_key=fcfa30881c3e984af96d841d25a29426
   }, []);
-
-  const inputHandleChange = e => {
-    setInputValue(e.currentTarget.value.toLowerCase());
-  };
-
-    const handleSubmit = e => {
-      e.preventDefault();
-      if (inputValue.trim() === '') {
-        Notify.info('Enter the name of the Movies 🎥 ');
-        return;
-      }
-      onSubmit(inputValue);
-      setInputValue('');
-    };
-
-  return (
-    <Header>
-      <Form onSubmit={handleSubmit}>
-        <Button type="submit">
-          <BsSearch />
-        </Button>
-
-        <Input
-          type="text"
-          autocomplete="off"
-          autoFocus
-          value={inputValue}
-          placeholder="Search for Movies and Series"
-          onChange={inputHandleChange}
-        />
-      </Form>
-    </Header>
-  );
+  return <div>Trending today 🤷‍♂️</div>;
 };
 
 export default Movies;
-
-Movies.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
