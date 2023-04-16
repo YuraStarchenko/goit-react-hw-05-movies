5a47e83074131c7880b0440e9072dcc276470df5
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-16 10:50:04 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect,  } from 'react';
 import { Div, List, Item } from './Home.styled.js';
 import { Container } from 'components/Container.styled.js';
 
@@ -9,15 +9,7 @@ const Home = () => {
       <Div>Trending Today 🔝</Div>
       <List>
         <Item>
-          <main>
-            <h1>Welcome</h1>
-            <img src="https://via.placeholder.com/960x240" alt="" />
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-              laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-              voluptatum voluptate.
-            </p>
-          </main>
+
         </Item>
       </List>
     </Container>
