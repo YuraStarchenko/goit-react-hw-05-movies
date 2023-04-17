5521a8550294afe1db279c97f27d19bcb31e1cf9
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-17 19:12:41 +0300

    +

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Div, LinkHome, List, Item, Icon } from './Home.styled.js';
+import { Div, LinkHome, List, Item, Img} from './Home.styled.js';
 import { Container } from 'components/Container.styled.js';
 import { getTrendingMuvies } from 'service/movieApi.jsx';
-import { RiMovie2Fill } from 'react-icons/ri';
 
 const Home = () => {
   const [trendingMuvies, setTrendingMuvies] = useState([]);
@@ -12,24 +11,33 @@ const Home = () => {
 
   useEffect(() => {
     try {
-      getTrendingMuvies().then(r => setTrendingMuvies([...r.results]));
+      getTrendingMuvies().then(r => setTrendingMuvies(r.results));
     } catch (error) {
       console.log(error);
     }
-  }, []);
+	}, []);
+
+  const viewPoster = poster_path => {
+    if (poster_path === null) {
+      return 'https://wipfilms.net/wp-content/data/posters/tt0338683.jpg';
+    }
+    return `https://image.tmdb.org/t/p/w300${poster_path}`;
+  };
 
   return (
     <Container>
       <Div>Trending Today 🔝</Div>
       <List>
-        {trendingMuvies.map(muvies => {
+        {trendingMuvies.map(({ id, poster_path, title }) => {
           return (
-            <Item key={muvies.id}>
-              <LinkHome to={`movies/${muvies.id}`} state={{ from: location }}>
-                <Icon>
-                  <RiMovie2Fill />
-                </Icon>
-                {muvies.title}
+            <Item key={id}>
+              <LinkHome to={`movies/${id}`} state={{ from: location }}>
+                  <Img
+                    src={`${viewPoster(poster_path)}`}
+                    alt={title}
+                    width="300"
+                    height="450"
+                  />                
               </LinkHome>
             </Item>
           );
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -8,25 +8,45 @@ export const Div = styled.div`
 `;
 
 export const List = styled.ul`
-  padding: 30px 0;
+  padding: 0;
+  margin: 0;
+  margin-top: 30px;
+  list-style: none;
+  gap: 20px;
+  margin-bottom: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+  }
 `;
 
 export const Item = styled.li`
-  margin-bottom: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
   border-radius: 10px;
-  padding: 10px;
+  box-shadow: 0 0 10px 1px #7bd4eb;
+  border-radius: 20px;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    box-shadow: 2px 2px 13px 4px #475569;
+    transform: scale(1.03);
+  }
 `;
 
 export const LinkHome = styled(Link)`
   text-decoration: none;
   font-weight: 700;
-  display: flex;
   text-transform: uppercase;
   :hover {
     color: orangered;
   }
 `;
 
-export const Icon = styled.div`
-  margin-right: 10px;
+export const Img = styled.img`
+  border-radius: 20px;
 `;
